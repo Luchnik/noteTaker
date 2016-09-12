ca101f7242a3a0075fcbf90a945e7b0a22a343de
Andriy Luchko <andruluchko@ukr.net>
2016-09-12 20:15:03 +0300

    Added Authentication service

--- js/app.js
+++ js/app.js
@@ -1,4 +1,5 @@
-var myApp = angular.module('myApp', ['ngRoute']);
+var myApp = angular.module('myApp', ['ngRoute', 'firebase'])
+  .constant('FIREBASE_URL', 'https://notetaker96.firebaseio.com/');
 
 myApp.config(['$routeProvider', function($routeProvider) {
   $routeProvider.
