f520701bf75b639e2e43e3cfc27c56f2cc486707
Andriy Luchko <andruluchko@ukr.net>
2016-09-12 20:45:52 +0300

    Added login ability and registration reference for users

--- js/services/authentication.js
+++ js/services/authentication.js
@@ -1,11 +1,19 @@
-myApp.factory('Authentication', ['$rootScope', '$firebaseAuth', 'FIREBASE_URL',
-  function($rootScope, $firebaseAuth, FIREBASE_URL) {
+myApp.factory('Authentication', ['$rootScope', '$firebaseAuth', '$location', 'FIREBASE_URL',
+  function($rootScope, $firebaseAuth, $location, FIREBASE_URL) {
 
   var ref = new Firebase(FIREBASE_URL);
   var auth = $firebaseAuth(ref);
 
   return {
     login: function(user) {
+      auth.$authWithPassword({
+        email: user.email,
+        password: user.password
+      }).then(function(regUser) {
+        $location.path('/success');
+      }).catch(function(error) {
+        $rootScope.message = error.message;
+      });
       $rootScope.message = 'Welcome ' + $scope.user.email;
     },
     register: function(user) {
@@ -13,6 +21,16 @@ myApp.factory('Authentication', ['$rootScope', '$firebaseAuth', 'FIREBASE_URL',
         email: user.email,
         password: user.password
       }).then(function(regUser) {
+
+        var regRef = new Firebase(FIREBASE_URL + 'users')
+        .child(regUser.uid).set({
+          date: Firebase.ServerValue.TIMESTAMP,
+          regUser: regUser.uid,
+          firstname: user.firstname,
+          lastname: user.lastname,
+          email: user.email
+        }); // user info
+
         $rootScope.message = 'Hi ' + user.firstname + ', Thanks for registering';
       }).catch(function(error) {
         $rootScope.message = error.message;
