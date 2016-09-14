65f83d0109154fea6b5eeebab57d41a1a4318229
Andriy Luchko <andruluchko@ukr.net>
2016-09-14 12:56:03 +0300

    Implement add note functionality

--- js/controllers/success.js
+++ js/controllers/success.js
@@ -1,3 +1,24 @@
-myApp.controller('SuccessController', ['$scope', function($scope) {
-  $scope.message = "success!!!";
+myApp.controller('SuccessController',
+  ['$scope', '$rootScope', '$firebaseAuth', '$firebaseArray', 'FIREBASE_URL',
+  function($scope, $rootScope, $firebaseAuth, $firebaseArray, FIREBASE_URL) {
+
+    var ref = new Firebase(FIREBASE_URL);
+    var auth = $firebaseAuth(ref);
+
+    auth.$onAuth(function(authUser) {
+      if (authUser) {
+        var notesRef = new Firebase(FIREBASE_URL + 'users/' + $rootScope.currentUser.$id + '/notes');
+        var notesInfo = $firebaseArray(notesRef);
+
+        $scope.addNote = function() {
+          notesInfo.$add({
+            name: $scope.notename,
+            date: Firebase.ServerValue.TIMESTAMP
+          }).then(function() {
+            $scope.notename = '';
+          });
+        } // add note
+      }
+    });
+
 }]);
